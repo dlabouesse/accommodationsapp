22ba674b5c70a1cd515855467728649219f78e5d
Dimitri Labouesse <dimitri.labouesse@telecom-em.eu>
2015-02-15 12:20:42 +0000

    Main layout improved and layout for flash messages added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,15 @@ class SessionsController < ApplicationController
       user = User.find_by_email(params[:email])
       if user && user.authenticate(params[:password])
           session[:user_id] = user.id                           #stores the id in the session
-          redirect_to session[:return_to] || user          #displays the user/show view
+          flash[:success] = "Welcome back " +user.first_name + "!" 
+
+          if session[:return_to]
+              return_to_path = session[:return_to]
+              session[:return_to] = nil                            #destroys the session to avoid unexpected behavior on reconnection
+              redirect_to return_to_path
+          else
+              redirect_to user
+          end
       else
           flash[:error] = "Invalid name/password combination."
           render 'new'		          		                      #shows the signin page again
