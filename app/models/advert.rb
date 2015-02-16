f5037a7f52de635aef0496ad9f9fe332823d138e
Dimitri Labouesse <dimitri.labouesse@telecom-em.eu>
2015-02-16 14:17:07 +0000

    Validation constraints for adverts and properties added

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -3,6 +3,11 @@ class Advert < ActiveRecord::Base
     belongs_to :property
     has_many :images, :dependent => :destroy
 
+    validates_presence_of :content, :rent, :nb_of_free_beds
+
+    validates :nb_of_free_beds, numericality: {only_integer: true, greater_than: 0}
+    validates :rent, numericality: {greater_than: 0}
+
     def property_address
         property.address
     end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,9 @@
 class Property < ActiveRecord::Base
     has_one :advert, :dependent => :destroy
     has_many :users
+
+    validates_presence_of :address, :kind, :nb_of_rooms, :nb_of_bathrooms
+
+    validates :nb_of_rooms, numericality: {only_integer: true, greater_than: 0}
+    validates :nb_of_bathrooms, numericality: {only_integer: true, greater_than: 0}
 end
